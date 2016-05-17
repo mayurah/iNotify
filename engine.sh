1314b58621706ae24ec0ee84271990f85817c081
Mayur Pipaliya <mayuraho@macbook-air.local>
2016-05-17 14:30:47 +0530

    exit patch

--- engine.sh
+++ engine.sh
@@ -4,11 +4,11 @@
 if [ -z "$1" ]
   then
     echo "inotify <title> $2"
-    end
+    exit
 fi
 if [ -z "$2" ]
   then
     echo "inotify $1 <message>"
-    end
+    exit
 fi
 osascript -e 'display notification "'$2'" with title "'$1'"'
